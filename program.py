45f3f67e0ec3fde48202128852fada3c03e29748
Ana Finotelli <finotelli2004@gmail.com>
2023-09-14 14:12:05 -0300

    range

---
+++ program.py
@@ -0,0 +1,9 @@
+# para criar o intervalo de 1 a 100 pode-se utilizar o comando "range" iniciando em 1
+#(continuação da outra linha) ja que o inicio é exato e terminando em 101 
+#(continuação) pois o fim é sempre um numero antes do valor indicado.
+
+soma = 0 
+for x in range(1, 101):
+    soma += x 
+print (soma)
+print('Fim do programa')
